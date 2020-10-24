456e2ce8bef8aec15cdaa91ca74e04792225ea10
George Thomas <georgefsthomas@gmail.com>
2020-10-24 18:21:54 +0100

    Work around permission error

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gpio::{sysfs::SysFsGpioInput, GpioIn, GpioValue};
-use std::{env, process::Command, thread::sleep, time::Duration};
+use std::{env, io, process::Command, thread::sleep, time::Duration};
 
 fn main() {
     let mut args = env::args();
@@ -13,9 +13,32 @@ fn main() {
     let cmd_args: Vec<String> = args.collect();
 
     let mut prev_button_val = GpioValue::High; // assume its off to begin with
-    let mut button = SysFsGpioInput::open(port).expect("Failed to open GPIO port");
+
+    /*
+    This works around a race condition - we have to wait for permissions to be set correctly by the OS.
+    I suspect this hasn't been caught because people usually just use 'sudo'.
+    We could submit a PR to 'gpio' but the bigger issue is that the whole interface is deprecated.
+        (https://github.com/mbr/gpio-rs/issues/2)
+    The 'gpio-cdev' crate uses the modern interface, but is a bit low-level. I've already burnt one pin playing with it.
+    */
+    let mut gpio = loop {
+        match SysFsGpioInput::open(port) {
+            Ok(x) => {
+                println!("Successfully opened GPIO {}", port);
+                break x;
+            }
+            Err(e) => match e.kind() {
+                io::ErrorKind::PermissionDenied => {
+                    println!("Permission error on GPIO {}, trying again", port);
+                    sleep(Duration::from_millis(100));
+                }
+                _ => panic!("Failed to open GPIO: {}", e),
+            },
+        }
+    };
+
     loop {
-        let button_val = button.read_value().expect("Failed to read from button");
+        let button_val = gpio.read_value().expect("Failed to read from button");
         match button_val {
             GpioValue::High => {}
             GpioValue::Low => {
@@ -29,7 +52,7 @@ fn main() {
             }
         }
         prev_button_val = button_val;
-        //TODO presumably there's some way to just block?
+        //TODO with the cdev interface we should just be able to block
         sleep(Duration::from_millis(100));
     }
 }
